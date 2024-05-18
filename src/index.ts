29937a0898a35013d011e6773dedddcd7d1b78d0
Julian Zapata <julianandreszb@gmail.com>
2024-05-17 22:59:33 -0500

    update package project name. update wrangler

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,70 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 import { Ai } from '@cloudflare/ai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-
-	// If you set another name in wrangler.toml as the value for 'binding',
-	// replace "AI" with the variable name you defined.
 	AI: any;
 }
 
-export default {
-	async fetch(request: Request, env: Env) {
-		const ai = new Ai(env.AI);
+const app = new Hono<Env>({ AI: 'AI' });
+app.use(
+	'/*',
+	cors({
+		origin: ['http://localhost:5173', 'https://ai-storytelling.pages.dev'],
+	}),
+);
 
-		const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
-				prompt: "What is the origin of the phrase Hello, World"
-			}
-		);
+app.post('/entry', async (c) => {
+	const messages = await c.req.json();
+	const ai = new Ai(c.env.AI);
+	// const res = await ai.run('@cf/mistral/mistral-7b-instruct-v0.1', { // ERROR 500
+	const res = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+		// OK
+		// const res = await ai.run('@cf/meta/llama-2-7b-chat-fp16', { // OK
+		// OK
+		messages,
+	});
 
-		const jsonResponse =  new Response(JSON.stringify(response));
-		jsonResponse.headers.set('Content-Type', 'application/json');
-		jsonResponse.headers.set('Access-Control-Allow-Origin', '*');
-		return jsonResponse;
-	},
-};
+	return c.json(res);
+});
+
+app.get('/image', async (c) => {
+	const ai = new Ai(c.env.AI);
+
+	const fallbackPrompt = 'tale kids';
+	const prompt = c.req.query('query') || fallbackPrompt;
+
+	const summaryResult = await ai.run('@cf/facebook/bart-large-cnn', {
+		input_text: prompt,
+	});
+
+	const imageResponse = await ai.run('@cf/bytedance/stable-diffusion-xl-lightning', {
+		// const imageResponse = await ai.run('@cf/lykon/dreamshaper-8-lcm', {
+		prompt: summaryResult.summary,
+		// prompt,
+	});
+
+	c.header('Content-Type', 'image/png');
+
+	return c.body(imageResponse);
+});
+
+app.get('/translate', async (c) => {
+	const ai = new Ai(c.env.AI);
+
+	const text = c.req.query('text') || 'hello';
+	const source_lang = c.req.query('source_lang') || 'english';
+	const target_lang = c.req.query('target_lang') || 'spanish';
+
+	const translationOutput = await ai.run('@cf/meta/m2m100-1.2b', {
+		text,
+		source_lang,
+		target_lang,
+	});
+
+	c.header('Content-Type', 'image/png');
+
+	return c.json(translationOutput);
+});
+
+export default app;
